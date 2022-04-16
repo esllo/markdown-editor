1d08c6286fbf074af531b66e9918b94390ed3bc0
esllo <master@esllo.com>
2022-04-16 12:26:13 +0900

    init

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+    tsconfigPaths(),
+  ],
+  base: '',
+  build: {
+    sourcemap: true,
+    outDir: 'dist',
+  },
+});
