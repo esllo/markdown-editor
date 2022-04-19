2160ea465dd9de41c91ef98b4901e9ca89c7785f
esllo <master@esllo.com>
2022-04-20 01:26:48 +0900

    update useCodeMirror, add heading, marker

--- src/hooks/useCodeMirror.ts
+++ src/hooks/useCodeMirror.ts
@@ -10,61 +10,164 @@ import { EditorSelection, EditorState, StateCommand, Text, Transaction } from '@
 import { EditorView, keymap } from '@codemirror/view';
 import { useCallback, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import download from '../lib/download';
 import { RootState } from '../modules';
 import { change, initialize, setEditorView } from '../modules/editor';
 
-const insertHeadFirst: StateCommand = ({ state, dispatch }) => {
-  console.log(state);
-};
+function getHeading(line: string) {
+  const headings = ['###### ', '##### ', '#### ', '### ', '## ', '# '];
+  for (const heading of headings) {
+    if (line.startsWith(heading)) {
+      return heading;
+    }
+  }
+  return '';
+}
 
-const insertBoldMarker: StateCommand = ({ state, dispatch }) => {
-  const changes = state.changeByRange((range) => {
-    const isBoldBefore = state.sliceDoc(range.from - 2, range.from) === '**';
-    const isBoldAfter = state.sliceDoc(range.to, range.to + 2) === '**';
-    const changes = [];
+function insertHeading(headingText: string): StateCommand {
+  return ({ state, dispatch }) => {
+    const changes = state.changeByRange((range) => {
+      let lineStart = 0;
+      const { text }: any = state.doc;
+      let line = '';
+      for (let i = 0; i < text.length; i++) {
+        line = text[i];
+        let lineEnd = lineStart + line.length + 1;
+        if (lineStart <= range.from && range.to < lineEnd) {
+          break;
+        }
+        lineStart = lineEnd;
+      }
+      const changes = [];
+      const heading = getHeading(line);
+      if (!heading || heading !== headingText) {
+        // add heading
+        changes.push({
+          from: lineStart,
+          to: lineStart + heading.length,
+          insert: Text.of([headingText]),
+        });
+        return {
+          changes,
+          range: EditorSelection.range(lineStart + headingText.length, lineStart + headingText.length),
+        };
+      }
+      // remove heading
+      changes.push({
+        from: lineStart,
+        to: lineStart + headingText.length,
+        insert: Text.of(['']),
+      });
 
-    changes.push(
-      isBoldBefore
-        ? {
-            from: range.from - 2,
-            to: range.from,
-            insert: Text.of(['']),
-          }
-        : {
-            from: range.from,
-            insert: Text.of(['**']),
-          },
+      return {
+        changes,
+        range: EditorSelection.range(lineStart, lineStart),
+      };
+    });
+
+    dispatch(
+      state.update(changes, {
+        scrollIntoView: true,
+        annotations: Transaction.userEvent.of('input'),
+      }),
     );
 
-    changes.push(
-      isBoldAfter
-        ? {
-            from: range.to,
-            to: range.to + 2,
-            insert: Text.of(['']),
-          }
-        : {
-            from: range.to,
-            insert: Text.of(['**']),
-          },
+    return true;
+  };
+}
+
+function insertMarker(marker: string, enter: boolean = false): StateCommand {
+  return ({ state, dispatch }) => {
+    const changes = state.changeByRange((range) => {
+      const markerLength = marker.length;
+      let isMarkerBefore = state.sliceDoc(range.from - markerLength, range.from) === marker;
+      let isMarkerAfter = state.sliceDoc(range.to, range.to + markerLength) === marker;
+      const changes = [];
+
+      if ((isMarkerBefore && !isMarkerAfter) || (!isMarkerBefore && isMarkerAfter)) {
+        isMarkerAfter = false;
+        isMarkerBefore = false;
+      }
+
+      const isAdditional = !isMarkerBefore && !isMarkerAfter && enter;
+
+      const beforeMarkers = [marker];
+      const afterMarkers = [marker];
+
+      let additional = 3;
+      if (isAdditional) {
+        changes.push({
+          from: range.to,
+          to: range.to,
+          insert: Text.of(['']),
+        });
+        const beforeText = state.sliceDoc(range.from - 1, range.to);
+        if (beforeText !== '' && beforeText !== '\n') {
+          beforeMarkers.unshift('');
+          additional++;
+        }
+        beforeMarkers.push('');
+        if (state.sliceDoc(range.from, range.to).length > 0) {
+          afterMarkers.unshift('');
+        }
+        afterMarkers.push('');
+      }
+
+      changes.push(
+        isMarkerBefore
+          ? {
+              from: range.from - markerLength,
+              to: range.from,
+              insert: Text.of(['']),
+            }
+          : {
+              from: range.from,
+              insert: Text.of(beforeMarkers),
+            },
+      );
+
+      changes.push(
+        isMarkerAfter
+          ? {
+              from: range.to,
+              to: range.to + markerLength,
+              insert: Text.of(['']),
+            }
+          : {
+              from: range.to,
+              insert: Text.of(afterMarkers),
+            },
+      );
+
+      const extendBefore = isMarkerBefore ? -markerLength : markerLength;
+      const extendAfter = isMarkerAfter ? -markerLength : markerLength;
+
+      if (isAdditional) {
+        return {
+          changes,
+          range: EditorSelection.range(range.from + additional, range.from + additional),
+        };
+      }
+
+      return {
+        changes,
+        range: EditorSelection.range(range.from + extendBefore, range.to + extendAfter),
+      };
+    });
+
+    dispatch(
+      state.update(changes, {
+        scrollIntoView: true,
+        annotations: Transaction.userEvent.of('input'),
+      }),
     );
 
-    const extendBefore = isBoldBefore ? -2 : 2;
-    const extendAfter = isBoldAfter ? -2 : 2;
+    return true;
+  };
+}
 
-    return {
-      changes,
-      range: EditorSelection.range(range.from + extendBefore, range.to + extendAfter),
-    };
-  });
-
-  dispatch(
-    state.update(changes, {
-      scrollIntoView: true,
-      annotations: Transaction.userEvent.of('input'),
-    }),
-  );
-
+const downloadMarkdown: StateCommand = ({ state }) => {
+  download('markdown.md', state.doc.toString());
   return true;
 };
 
@@ -98,12 +201,60 @@ const createEditorState = (doc: string, handleDocChange: HandleDocChange) => {
     extensions: [
       keymap.of([
         {
+          key: 'Ctrl-b',
+          run: insertMarker('**'),
+        },
+        {
           key: 'Ctrl-i',
-          run: insertBoldMarker,
+          run: insertMarker('*'),
+        },
+        {
+          key: 'Ctrl-d',
+          run: insertMarker('~~'),
+        },
+        {
+          key: 'Ctrl-`',
+          run: insertMarker('`'),
+          preventDefault: true,
+        },
+        {
+          key: 'Ctrl-Shift-`',
+          run: insertMarker('```', true),
+          preventDefault: true,
         },
         {
           key: 'Ctrl-1',
-          run: insertHeadFirst,
+          run: insertHeading('# '),
+          preventDefault: true,
+        },
+        {
+          key: 'Ctrl-2',
+          run: insertHeading('## '),
+          preventDefault: true,
+        },
+        {
+          key: 'Ctrl-3',
+          run: insertHeading('### '),
+          preventDefault: true,
+        },
+        {
+          key: 'Ctrl-4',
+          run: insertHeading('#### '),
+          preventDefault: true,
+        },
+        {
+          key: 'Ctrl-5',
+          run: insertHeading('##### '),
+          preventDefault: true,
+        },
+        {
+          key: 'Ctrl-6',
+          run: insertHeading('###### '),
+          preventDefault: true,
+        },
+        {
+          key: 'Ctrl-s',
+          run: downloadMarkdown,
           preventDefault: true,
         },
         ...historyKeymap,
@@ -126,6 +277,12 @@ const createEditorState = (doc: string, handleDocChange: HandleDocChange) => {
           handleDocChange(update.state.doc.toString());
         }
       }),
+      EditorView.theme({
+        '.cm-content': {
+          fontFamily:
+            '-apple-system, BlinkMacSystemFont, "Helvetica Neue", "Apple SD Gothic Neo", "Malgun Gothic", "맑은 고딕", 나눔고딕, "Nanum Gothic", "Noto Sans KR", sans-serif',
+        },
+      }),
     ],
   });
 };
@@ -140,7 +297,6 @@ const useCodeMirror = () => {
   }, []);
 
   useEffect(() => {
-    console.log(refContainer.current, initialized);
     if (!refContainer.current || initialized) return;
 
     const state = createEditorState('', handleDocChange);
